8672343b73241fb19e3d3c7b2403a335d51fdb87
CrystalxBlade <satish141m@gmail.com>
2026-02-12 16:57:10 +0530

    newforce

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,6 +4,8 @@ public class Ball : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] float moveForce;
+    [SerializeField] float jumpForce;
+    bool isGrounded = false;
     float xInput, zInput;
     void Start()
     {
@@ -14,10 +16,26 @@ public class Ball : MonoBehaviour
         xInput = Input.GetAxis("Horizontal");
         zInput = Input.GetAxis("Vertical");
         Move();
-    }
 
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false;
+            Jump();
+        }
+    }
     void Move()
     {
         rb.AddForce(new Vector3(xInput, 0, zInput) * moveForce);
+    }
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag(""))
+        {
+            isGrounded = true;
+        }
     }
 }
