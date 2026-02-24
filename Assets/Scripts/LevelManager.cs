ecf4505ed0bb2345b48bcdf93f259bf1cf6f1732
CrystalxBlade <satish141m@gmail.com>
2026-02-24 16:31:28 +0530

    finish

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,20 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    [SerializeField] int nextLevel;
     void Start()
     {
-        
+        nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
     }
     void Update()
     {
         
+    }
+    void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Water"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Invoke("Reload", 4f);
+        }
+        if(other.gameObject.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene(nextLevel);
         }
     } 
 }
